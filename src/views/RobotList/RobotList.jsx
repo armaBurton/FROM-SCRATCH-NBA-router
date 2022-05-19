81da34c28eae1f6256dece4e035057cef5eccbe5
armaBurton <armanarma@armaburton.com>
2022-05-18 20:27:54 -0700

    page state remains on refresh

--- src/views/RobotList/RobotList.jsx
+++ src/views/RobotList/RobotList.jsx
@@ -4,64 +4,62 @@ import RobotCard from '../RobotCard/RobotCard';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import robotFetch from '../../services/robotFetch';
+import { func } from 'prop-types';
 
-export default function RobotList(){
+export default function RobotList() {
   const location = useLocation();
-  const searchGender = new URLSearchParams(location.search).get('gender') || 'all';
+  const searchGender = new URLSearchParams(location.search).get('gender');
   console.log(searchGender);
   const history = useHistory();
-  
-  const {
-    loading, setLoading,
-    robots, setRobots,
-    gender, setGender,
-  } = useRUARobotContext();
+  const storage = JSON.parse(localStorage.getItem('robots'));
+  console.log(`|| storage >`, storage);
 
-  const handleGenderChange = (e) => {
+  const { loading, setLoading, robots, setRobots, gender, setGender } =
+    useRUARobotContext();
+
+  // async function getNewRobots(e) {
+  //   return await robotFetch(
+  //     `https://randomuser.me/api/?results=10&noinfo&gender=${e.target.value}`
+  //   );
+  // }
+
+  const handleGenderChange = async (e) => {
     e.preventDefault();
     setGender(e.target.value);
-    setRobots([]);
+
+    const getRobots = await robotFetch(
+      `https://randomuser.me/api/?results=10&noinfo&gender=${e.target.value}`
+    );
+
+    localStorage.setItem('robots', JSON.stringify(getRobots.results));
     history.push(`/robots/?gender=${e.target.value}`);
   };
 
-  useEffect(() => {
-    async function getRobots() {
-      setLoading(true);      
-
-      const url = searchGender === 'all'
-        ? 'https://randomuser.me/api/?results=10&noinfo'
-        : `https://randomuser.me/api/?results=10&noinfo&gender=${searchGender}`;
-
-      let results = '';
-
-      !robots.length
-        ? {results} = await robotFetch(url)
-        : results = robots;
-
-      setLoading(false);
-      setRobots(results);
-    }
-
-    getRobots();
-  }, [gender])
+  function resetRobots() {
+    localStorage.clear();
+    history.push('/');
+  }
 
   return (
-      <section className={style.meetMyRobots}>
-          <div className={style.listHead}>
-            <label htmlFor='gender'>ManDroid or FemBots</label>
-            <select id="gender" value={gender} onChange={handleGenderChange}>
-              <option value="all">ALL</option>
-              <option value="male">ManDroid</option>
-              <option value="female">FemBot</option>
-            </select>
-          </div>
-        <div className={style.cards}>
-          {
-            loading 
-              ? <h1>Beep Beep Boop...Computing</h1> 
-            : robots.map((robot, i) => <RobotCard key={`${robot}${i}`} robot={robot} i={i}  />)
-          }
-        </div>
-      </section>
+    <section className={style.meetMyRobots}>
+      <div className={style.listHead}>
+        <button onClick={resetRobots}>Reset</button>
+        <label htmlFor="gender">ManDroid or FemBots</label>
+        <select id="gender" value={searchGender} onChange={handleGenderChange}>
+          <option value="all">ALL</option>
+          <option value="male">ManDroid</option>
+          <option value="female">FemBot</option>
+        </select>
+      </div>
+      <div className={style.cards}>
+        {loading ? (
+          <h1>Beep Beep Boop...Computing</h1>
+        ) : (
+          storage.map((robot, i) => (
+            <RobotCard key={`${robot}${i}`} robot={robot} i={i} />
+          ))
+        )}
+      </div>
+    </section>
   );
 }
